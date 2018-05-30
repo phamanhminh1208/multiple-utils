dbb0bd8eae77152518c5016f1635cbd3af18b0ca
MinhPA-HPLAP <phamanhminh1208@gmail.com>
2018-05-30 22:11:21 +0700

    - improve getDateOnly

--- common-utils/src/main/java/com/that2u/android/app/utils/DateTimeUtil.java
+++ common-utils/src/main/java/com/that2u/android/app/utils/DateTimeUtil.java
@@ -1,6 +1,5 @@
 package com.that2u.android.app.utils;
 
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
@@ -12,17 +11,21 @@ import java.util.Date;
 public final class DateTimeUtil {
     private static final SimpleDateFormat sDateOnlyFormat = new SimpleDateFormat("dd/MM/yyyy");
 
-    public static String getDateStringOnlyFromMillisecond(long millisecond){
-        Date date = new Date(millisecond);
-        return sDateOnlyFormat.format(date);
+    public static Calendar getDateStringOnlyFromMillisecond(long millisecond){
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(millisecond);
+        calendar.set(Calendar.HOUR, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+        return calendar;
     }
 
     public static Date getDateOnlyFromMillisecond(long millisecond){
-        String dateStr = getDateStringOnlyFromMillisecond(millisecond);
+        Calendar calendar = getDateStringOnlyFromMillisecond(millisecond);
         try {
-
-            return sDateOnlyFormat.parse(dateStr);
-        } catch (ParseException e) {
+            return calendar.getTime();
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return null;
