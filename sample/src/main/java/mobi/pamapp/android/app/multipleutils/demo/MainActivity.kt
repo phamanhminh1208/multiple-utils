2c3ecd03d5a7539fff66506fbb0202ceaa45ab83
phamanhminh1208 <phamanhminh1208@gmail.com>
2018-06-02 22:33:49 +0700

    - update sample

--- sample/src/main/java/mobi/pamapp/android/app/multipleutils/demo/MainActivity.kt
+++ sample/src/main/java/mobi/pamapp/android/app/multipleutils/demo/MainActivity.kt
@@ -1,7 +1,9 @@
 package mobi.pamapp.android.app.multipleutils.demo
 
-import android.support.v7.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.Gravity
 import android.view.View
 import cn.refactor.lib.colordialog.PromptDialog
 import kotlinx.android.synthetic.main.activity_main.*
@@ -20,6 +22,9 @@ class MainActivity : AppCompatActivity() {
                     .setContentText("This is a demo dialog")
                     .setPositiveListener("Yes", PromptDialog.OnButtonListener { dialog -> dialog.dismiss() })
                     .setNegativeListener("No", PromptDialog.OnButtonListener { dialog -> dialog.dismiss() })
+                    .setPositiveDrawable(R.drawable.abc_btn_check_material)
+                    .setContentTextAlignment(Gravity.CENTER)
+                    .setPositiveTextColor(Color.RED)
                     .setButtonCloseVisible(true)
                     .setCancelable(false, false)
                     .showDialog()
